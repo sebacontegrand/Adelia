4c271367f2ab5d22221a4fc8b176fa9e5d496e9a
Sebastian Conte-Grand <sebastianconte-grand@mac.lan>
2026-01-26 11:30:50 -0300

    db env

--- firebase/firebase.config.ts
+++ firebase/firebase.config.ts
@@ -6,7 +6,7 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional with next public variables
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
